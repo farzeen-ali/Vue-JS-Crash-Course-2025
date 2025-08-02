0474e811323402da89e47c6244c1c35631f4c4f9
farzeen-ali <farzeenaliarif786@gmail.com>
2025-08-02 11:38:49 +0500

    Life Cycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,16 @@
       <!-- <EventHandling /> -->
        <!-- <LocalGlobalComponents /> -->
         <!-- <Props name="Farzeen Ali" email="farzeen@gmail.com" /> -->
-         <EmittingEvents @notify="handleNotify" />
-         <p v-if="message">{{ message }}</p>
+         <!-- <EmittingEvents @notify="handleNotify" />
+         <p v-if="message">{{ message }}</p> -->
+         <LifeCycleHooks />
   </div>
 </template>
 
 <script setup>
-import EmittingEvents from './components/EmittingEvents.vue';
+import LifeCycleHooks from './components/LifeCycleHooks.vue';
+
+// import EmittingEvents from './components/EmittingEvents.vue';
 // import Props from './components/Props.vue';
 // import TemplateSyntax from './components/TemplateSyntax.vue'
 // import VBind from './components/VBind.vue';
@@ -25,12 +28,12 @@ import EmittingEvents from './components/EmittingEvents.vue';
 // import Methods from './components/Methods.vue';
 // import EventHandling from './components/EventHandling.vue';
 // import LocalGlobalComponents from './components/LocalGlobalComponents.vue';
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
-const message = ref('')
+// const message = ref('')
 
-function handleNotify(msg) {
-  message.value = msg
-}
+// function handleNotify(msg) {
+//   message.value = msg
+// }
 </script>
 
---
+++ src/components/LifeCycleHooks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Dashboard</h2>
+    <p v-if="loading">Loading data...</p>
+    <p v-else>{{ data }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const data = ref('')
+const loading = ref(true)
+
+onMounted(async () => {
+  // Simulated API call
+  await new Promise(resolve => setTimeout(resolve, 1500))
+  data.value = 'Welcome Farzeen, your dashboard is ready!'
+  loading.value = false
+})
+</script>
