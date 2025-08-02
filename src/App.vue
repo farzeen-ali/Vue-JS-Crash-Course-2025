3e08740c39dcb442e04e9d2ebb8ff0ac6200b678
farzeen-ali <farzeenaliarif786@gmail.com>
2025-08-02 11:35:50 +0500

    Emitting Events

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@
       <!-- <Methods /> -->
       <!-- <EventHandling /> -->
        <!-- <LocalGlobalComponents /> -->
-        <Props name="Farzeen Ali" email="farzeen@gmail.com" />
+        <!-- <Props name="Farzeen Ali" email="farzeen@gmail.com" /> -->
+         <EmittingEvents @notify="handleNotify" />
+         <p v-if="message">{{ message }}</p>
   </div>
 </template>
 
 <script setup>
-import Props from './components/Props.vue';
-
+import EmittingEvents from './components/EmittingEvents.vue';
+// import Props from './components/Props.vue';
 // import TemplateSyntax from './components/TemplateSyntax.vue'
 // import VBind from './components/VBind.vue';
 // import VModel from './components/VModel.vue';
@@ -23,6 +25,12 @@ import Props from './components/Props.vue';
 // import Methods from './components/Methods.vue';
 // import EventHandling from './components/EventHandling.vue';
 // import LocalGlobalComponents from './components/LocalGlobalComponents.vue';
+import { ref } from 'vue'
 
+const message = ref('')
+
+function handleNotify(msg) {
+  message.value = msg
+}
 </script>
 
---
+++ src/components/EmittingEvents.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="sendMessage">Click Me</button>
+</template>
+
+<script setup>
+const emit = defineEmits(['notify'])
+
+function sendMessage() {
+  emit('notify', 'Hello from Child!')
+}
+</script>
