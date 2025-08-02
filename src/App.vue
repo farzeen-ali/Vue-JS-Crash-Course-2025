8af8e058b818c253505726f5eb46b8afcab43e9e
farzeen-ali <farzeenaliarif786@gmail.com>
2025-08-02 11:15:56 +0500

    Conditional Rendering

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <div>
     <!-- <TemplateSyntax /> -->
      <!-- <VBind /> -->
-     <VModel />
+     <!-- <VModel /> -->
+      <ConditionalRendering />
   </div>
 </template>
 
 <script setup>
 // import TemplateSyntax from './components/TemplateSyntax.vue'
 // import VBind from './components/VBind.vue';
-import VModel from './components/VModel.vue';
+// import VModel from './components/VModel.vue';
+import ConditionalRendering from './components/ConditionalRendering.vue';
 </script>
 
---
+++ src/components/ConditionalRendering.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p v-if="status === 'loggedIn'">Welcome back, Farzeen!</p>
+    <p v-else-if="status === 'loading'">Checking your session...</p>
+    <p v-else>Please log in to continue.</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const status = ref('loggedIn') // try: 'loading' or 'guest'
+</script>
