8b5e7c164637ead00626cbb8afb5f2c18645b6ef
farzeen-ali <farzeenaliarif786@gmail.com>
2025-08-02 11:21:52 +0500

    Event Handling

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
      <!-- <VModel /> -->
       <!-- <ConditionalRendering /> -->
       <!-- <VFor /> -->
-      <Methods />
+      <!-- <Methods /> -->
+      <EventHandling />
   </div>
 </template>
 
@@ -15,6 +16,7 @@
 // import VModel from './components/VModel.vue';
 // import ConditionalRendering from './components/ConditionalRendering.vue';
 // import VFor from './components/VFor.vue';
-import Methods from './components/Methods.vue';
+// import Methods from './components/Methods.vue';
+import EventHandling from './components/EventHandling.vue';
 </script>
 
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input v-model="name" placeholder="Enter your name" />
+    <button @click="handleSubmit">Submit</button>
+    <p v-if="greeting">{{ greeting }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+const greeting = ref('')
+
+function handleSubmit() {
+  if (name.value.trim()) {
+    greeting.value = `Hello, ${name.value}! 👋`
+  } else {
+    greeting.value = 'Please enter your name first.'
+  }
+}
+</script>
