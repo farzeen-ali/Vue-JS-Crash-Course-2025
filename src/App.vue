11d1018ce7f3e3c71041fb24f55eaea9c61218a0
farzeen-ali <farzeenaliarif786@gmail.com>
2025-08-02 11:10:49 +0500

    V-Bind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <TemplateSyntax />
+    <!-- <TemplateSyntax /> -->
+     <VBind />
   </div>
 </template>
 
 <script setup>
-import TemplateSyntax from './components/TemplateSyntax.vue'
+// import TemplateSyntax from './components/TemplateSyntax.vue'
+import VBind from './components/VBind.vue';
 </script>
 
---
+++ src/components/VBind.vue
@@ -0,0 +1,11 @@
+<template>
+  <button :title="tooltipText">
+    Submit
+  </button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const tooltipText = ref('Click to submit your response')
+</script>
